b48ec4a2fbd2accbb1cf2a9685d93c77c2829ff7
aphuus <aphuus@utu.fi>
2024-10-22 14:41:38 +0300

    Setup appwrite and database

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,23 +1,53 @@
 import { Hono } from "hono";
+import { deleteCookie, setCookie } from "hono/cookie";
+import { ID } from "node-appwrite";
 
 import { zValidator } from "@hono/zod-validator";
+import { createAdminClient } from "@/lib/appwrite";
 
 import { loginSchema, registerSchema } from "../schemas";
+import { AUTH_COOKIE } from "../constants";
 
 export const app = new Hono()
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
-    console.log({ email, password });
+    const { account } = await createAdminClient();
+    const session = await account.createEmailPasswordSession(email, password);
 
-    return c.json({ email, password });
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      expires: new Date(session.expire),
+      maxAge: 30 * 24 * 60 * 60,
+    });
+
+    return c.json({ success: true });
   })
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
 
-    console.log({ name, email, password });
+    const { account } = await createAdminClient();
+    await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
 
-    return c.json({ name, email, password });
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      expires: new Date(session.expire),
+      maxAge: 30 * 24 * 60 * 60,
+    });
+
+    return c.json({ success: true });
+  })
+  .post("/logout", async (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({ success: true });
   });
 
 export default app;
