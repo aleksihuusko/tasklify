8291f0c894da31bdc5b65773078014a6454197c5
aphuus <aphuus@utu.fi>
2024-10-22 08:47:17 +0300

    Fixed eslint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl font-bold">Tasklify</h1>
+      <h1 className="text-4xl font-semibold">Tasklify</h1>
+      <Input placeholder="Enter your task" />
     </main>
   );
 }
